0f54c2fc7f7603cd1fe85a8c7837b2a03d5af10f
vladzur <vladzur@gmail.com>
2024-07-29 23:44:19 -0400

    shaketree config

--- src/main.js
+++ src/main.js
@@ -5,16 +5,11 @@ import { createPinia } from 'pinia'
 // Vuetify
 import 'vuetify/styles'
 import { createVuetify } from 'vuetify'
-import * as components from 'vuetify/components'
-import * as directives from 'vuetify/directives'
 
 import App from './App.vue'
 import router from './router'
 
-const vuetify = createVuetify({
-  components,
-  directives
-})
+const vuetify = createVuetify()
 
 const app = createApp(App)
 
