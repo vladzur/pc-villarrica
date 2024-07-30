82ba259674e3b842fd86c8d83b9750c9f9386e73
vladzur <vladzur@gmail.com>
2024-07-30 00:11:20 -0400

    footer refactor

--- src/components/FooterComponent.vue
+++ src/components/FooterComponent.vue
@@ -1,9 +1,32 @@
+<script setup>
+const goToInstagram = () => {
+    window.open('https://www.instagram.com/partido.comunista.villarrica/', '_blank');
+}
+const goToFacebook = () => {
+    window.open('https://www.facebook.com/people/PC-Villarrica/61552411023948/', '_blank');
+}
+const goToYoutube = () => {
+    window.open('https://www.youtube.com/@PCVillarrica', '_blank');
+}
+
+</script>
 <template>
-    <v-footer color="primary" app>
-        <v-col>
-            <v-row>
-                <p>© 2024 - PC Villarrica</p>
-            </v-row>
-        </v-col>
+    <v-footer padless height="auto" color="red" class="text-center">
+        <v-row class="align-center justify-center">
+            <v-col cols="12" class="py-4">
+                <v-icon color="white" class="mx-2" @click="goToInstagram">
+                    <v-icon icon="mdi-instagram" />
+                </v-icon>
+                <v-icon color="white" class="mx-2" @click="goToFacebook">
+                    <v-icon icon="mdi-facebook" />
+                </v-icon>
+                <v-icon color="white" class="mx-2" @click="goToYoutube">
+                    <v-icon icon="mdi-youtube" />
+                </v-icon>
+            </v-col>
+            <v-col cols="12" class="py-4">
+                <span class="white--text">&copy; {{ new Date().getFullYear() }} - PC Villarrica</span>
+            </v-col>
+        </v-row>
     </v-footer>
 </template>
--- src/main.js
+++ src/main.js
@@ -1,15 +1,24 @@
-import './assets/main.css'
+import '@mdi/font/css/materialdesignicons.css'
 
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
 // Vuetify
 import 'vuetify/styles'
 import { createVuetify } from 'vuetify'
+import { aliases, mdi } from 'vuetify/iconsets/mdi'
 
 import App from './App.vue'
 import router from './router'
 
-const vuetify = createVuetify()
+const vuetify = createVuetify({
+  icons: {
+    defaultSet: 'mdi',
+    aliases,
+    sets: {
+      mdi
+    }
+  }
+})
 
 const app = createApp(App)
 
